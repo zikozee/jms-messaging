d0bfe920a52dbfccbc63c45c3f784ebe4a2dd652
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-02-05 15:46:02 +0100

    message Converter implmented. closes #3

---
+++ src/main/java/com/zikozee/jms_messaging/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package com.zikozee.jms_messaging.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    //POJO <-> Object <=> JSON
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
