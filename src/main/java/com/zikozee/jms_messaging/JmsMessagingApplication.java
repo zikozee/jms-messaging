2f7e5a8c4a1ded14ce588b3b13f29d87018f5094
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-02-05 14:40:27 +0100

    Initial commit

---
+++ src/main/java/com/zikozee/jms_messaging/JmsMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.zikozee.jms_messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsMessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zikozee/jms_messaging/JmsMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zikozee.jms_messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
